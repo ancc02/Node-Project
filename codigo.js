49aac4ba67fbe5001201c0daf6d81f0cfedda7fb
ancc02 <oandresccc@gmail.com>
2022-09-25 12:23:18 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,13 @@
+// var dias = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+// let contacto = { "nombre": "julieta", "email": "julieta@gmail.com", "edad": 21 };
+
+// console.log(contacto.nombre);
+
+
+// var colors = require('colors');
+// var prueba = require('./funciones/funcion1');
+// let contacto = { "nombre": "julieta", "email": "julieta@gmail.com", "edad": 21 };
+//console.log(contacto.email);
+
+var prueba = require('./funciones/funcion1');
+prueba.resta(5,9);
